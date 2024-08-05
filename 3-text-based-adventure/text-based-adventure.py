7a00e437265c11ee4f73fcd8d4d2c76a207eecd2
Shivi Vats <shivivats7@gmail.com>
2024-08-05 22:38:04 +0200

    more flair in the text adventure

--- 3-text-based-adventure/text-based-adventure.py
+++ 3-text-based-adventure/text-based-adventure.py
@@ -1,60 +1,103 @@
 def fleeText():
-  print("Scared from the growls, you fled back on the path through the woods, where Silas and Brangar await you.")
-  print("You can already imagine their disappointed faces as you'll share this with them")
+  print("Scared from the growls coming from the cave, you fled back on the path through the woods, where Silas and Brangar await you.")
+  print("You can already imagine their disappointed faces as you'll share this news with them.")
   print("The end.")
 
 
 def winText():
-  print("You carry him out of the cave without anyone else noticing and make your way back to Silas and Brangar, awaiting the relief on their faces when they see their companion brought back safely.")
-  print("The end")
+  print("You carry Gundren out of the cave without anyone else noticing and make your way back to Silas and Brangar, awaiting the relief on their faces when they see their companion brought back safely.")
+  print("The end.")
 
 # The idea is to describe situations, take user input and describe what happens
 # Do this in a loop and eventually end the game
 
+import random
+
+# Ask the player for their name
+print("What is your name?")
+player_name = input("> ")
+
 # Print a welcome message
-print("You see a cave in front of you. Moss grows on the rocky entrance, and sounds of running water and animal growls can be heard from the inside.")
-print("You see a wooden path disappear into the cave. Your vision is limited to a few meters.")
-print("Would you like to explore the cave or flee?")
+print("Welcome to Cragmaw Hideout, " + player_name + ".....")
+print("Following a forest trail, you come across a large cave in a hillside.")
+print("A shallow stream flows out of the cave mouth, which is screened by dense shrubs.")
+print("A narrow dry path leads into the cave on the right-hand side of the stream.")
+print("Would you like to go towards the cave or flee?")
 
 # Wait for user input
-userChoice = input("> ")
+entrance_choice = input("> ")
 
 # Do something based on the user's input
-if(userChoice == "explore the cave"): # or userChoice == "explore"
-  print("Some text here about the river and other visual descriptions of the cave...")
-  print("You see a room with two wolves leashed inside it. They are yanking their chains trying to reach a figure tied up in the corner, but the leashes are letting the person survive.... for now")
-  print("By the mark on their clothes, you can identify the prisoner as Gundren Rockseeker, the person you were sent here to rescue.")
-  print("You can walk around the wolves and try to flee Gundren, distract the wolves with food and try to free Gundren, or you can still flee.")
-  print("What would you like to do?")
+if(entrance_choice == "go towards the cave"): # or entrance_choice == "go towards"
+  print("As you approach the cave, you notice a small area in the shrubs on the east side of the stream has been hollowed out to form a lookout.")
+  print("Wooden planks flatten out the shrubs and provide room for guards to lie hidden and watch the area—including a pair of goblins lurking there right now!")
+  print("Would you like to go into the cave, try to ambush the goblins, or flee?")
 
-  caveChoice = input("> ")
+  # print("You see a room with two wolves leashed inside it. They are yanking their chains trying to reach a figure tied up in the corner, but the leashes are letting the person survive.... for now")
+  # print("By the mark on their clothes, you can identify the prisoner as Gundren Rockseeker, the person you were sent here to rescue.")
+  # print("You can walk around the wolves and try to flee Gundren, distract the wolves with food and try to free Gundren, or you can still flee.")
+  # print("What would you like to do?")
 
-  if(caveChoice == "walk around the wolves"): # or caveChoice == "walk around"
-    print("You successfully walk around the wolves and manage to free Gundren.")
-    winText()
+  thicket_choice = input("> ")
+  if(thicket_choice == "go into the cave"):
+    # stealthRoll = random.randint(1, 20)
+    # if(stealthRoll > 10):
+    #   print("You sneak into the cave mouth without the goblins noticing you.")
+    # else:
+    #   print("The goblins spot you, you are dead.")
+    print("The goblins spot you and fire their arrows.")
+    print("One of them finds its mark, you are dead.")
+    print("The end.")
 
-  elif(caveChoice == "distract the wolves with food"): # or caveChoice == "distract" or caveChoice = "distract with food"
-    print("You throw some of your rations at the wolves. The seemingly starved animals quickly leap to grab and begin to devour it.")
-    print("The wolves take a liking to you and let you walk across the chamber and free Gundren.")
-    winText()
+  elif(thicket_choice == "ambush the goblins"):
+    print("You sneak around the goblins and quickly shoot two arrows at them.")
+    print("Unaware of your presence, they have no time to react and are quickly felled.")
 
-  elif(caveChoice == "flee"):
-    fleeText()
+    print("Would you like to go into the cave?")
+    cave_choice = input("> ")
+
+    if(cave_choice == "yes"): # or cave_choice == "go into the cave"
+      print("Just inside the cave mouth, a few uneven stone steps lead up to a small, dank chamber on the east side of the passage.")
+      print("The cave narrows to a steep fissure at the far end, and is filled with the stench of animals.")
+      print("Savage snarls and the sounds of rattling chains greet your ears where three wolves are chained up just inside the opening.")
+      print("Each wolf's chain leads to an iron rod driven into the base of a stalagmite.")
+      print("They are yanking their chains trying to reach a figure tied up in the corner, but the leashes are letting the person survive.... for now")
+      print("By the mark on their clothes, you can identify the prisoner as Gundren Rockseeker, the person you were sent here to rescue.")
+      print("You can walk around the wolves and try to flee Gundren, distract the wolves with food and try to free Gundren, or you can still flee.")
+      print("What would you like to do?")
+
+      gundren_choice = input("> ")
+      if(gundren_choice == "walk around the wolves"): # or cave_choice == "walk around"
+        print("You successfully walk around the wolves and manage to free Gundren.")
+        winText()
+
+      elif(gundren_choice == "distract the wolves with food"): # or cave_choice == "distract" or cave_choice = "distract with food"
+        print("You throw some of your rations at the wolves. The seemingly starved animals quickly leap to grab and begin to devour it.")
+        print("You use this distraction and quickly make your way to Gundren, freeing him.")
+        winText()
+
+      elif(gundren_choice == "flee"):
+        fleeText()
+
+      else:
+        print("Invalid choice!.")
+
+    else:
+      fleeText()
 
   else:
-    print("Invalid choice! Please enter one of the choices described above.")
+    fleeText()
 
-
-elif(userChoice == "flee"):
+elif(entrance_choice == "flee"):
   fleeText()
 
-
 else:
-  print("Invalid choice! Please enter one of the choices described above.")
+  print("Invalid choice!.")
 
 
 # TODO:
 # make the invalid choice into a function too
+# make the invalid choice more consistent
 # make the game start again after an invalid choice
 # make each "room" into a class and use functions to show the text
 # ask the user for their name at the beginning and display that name
